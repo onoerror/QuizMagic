c23b9d67d2658295da0da683aa4ea89d2f010832
onoerror <a3575131c@gmail.com>
2016-03-11 11:44:08 +0800

    按鈕功能已設定字串

--- app/src/main/java/com/quizmagic/quizmagic/QuizMagic.java
+++ app/src/main/java/com/quizmagic/quizmagic/QuizMagic.java
@@ -1,7 +1,9 @@
 package com.quizmagic.quizmagic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class QuizMagic extends AppCompatActivity {
 
@@ -9,5 +11,39 @@ public class QuizMagic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_magic);
+
+    }
+
+    public void mu(View view) {
+        //mu是一個變數
+        TextView mu = (TextView)findViewById(R.id.textany);
+        //文字檔案 變數 = (轉文字檔)
+        mu.setText("你沒有這個命!");
+    }
+
+
+    public void st(View view) {
+        TextView mu = (TextView)findViewById(R.id.textany);
+        mu.setText("如果愚笨也算是一種屬性的話...");
+    }
+
+    public void nz(View view) {
+        TextView mu = (TextView)findViewById(R.id.textany);
+        mu.setText("麻煩去旁邊吃土好嗎，謝謝");
+    }
+
+    public void ju(View view) {
+        TextView mu = (TextView)findViewById(R.id.textany);
+        mu.setText("聽說世界上有一種生物叫做孬種");
+    }
+
+    public void jz(View view) {
+        TextView mu = (TextView)findViewById(R.id.textany);
+        mu.setText("這是我這輩子聽過最好笑的笑話了，哈哈哈");
+    }
+
+    public void out(View view) {
+        TextView mu = (TextView)findViewById(R.id.textany);
+        mu.setText("很抱歉，沒有離開這個選項  /(030)/");
     }
 }
